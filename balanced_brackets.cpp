b8350ca3fea63217f90a4b7b65b3c3d1cb8b7e81
deepakdhaked <deepakdhakedpc@gmail.com>
2022-08-11 04:31:14 +0530

    initial

--- balanced_brackets.cpp
+++ balanced_brackets.cpp
@@ -1,23 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-unordered_map<char, int> symbols = {{'[', -1}, {'{', -2}, {'(', -3},{']', 1}, {'}', 2}, {')', 3}};
-bool isBalanced(string s){
+unordered_map<char, int> symbols = {{'[', -1}, {'{', -2}, {'(', -3}, {']', 1}, {'}', 2}, {')', 3}};
+bool isBalanced(string s)
+{
     stack<char> st;
-    for(char brackets: s){
-        if(symbols[brackets]<0){
+    for (char brackets : s)
+    {
+        if (symbols[brackets] < 0)
+        {
             st.push(brackets);
         }
-        else{
-            if(st.empty()){
+        else
+        {
+            if (st.empty())
+            {
                 return false;
             }
             char top = st.top();
             st.pop();
-            if(symbols[top]+symbols[brackets]!=0){
+            if (symbols[top] + symbols[brackets] != 0)
+            {
                 return false;
             }
-
         }
     }
     return st.empty();
